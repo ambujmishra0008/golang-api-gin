22b38196698ac851c559180fbf5b1414ba77fa58
ambuj501 <ambujmishra0008@gmail.com>
2022-11-21 20:49:27 +0530

    first comment

--- main.go
+++ main.go
@@ -1,89 +1,81 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	actions "instasafe/action"
+	"instasafe/env"
+	"instasafe/logs"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"golang.org/x/sync/errgroup"
 )
 
-type todo struct {
-	ID        string `json:"id"`
-	Item      string `json:"item"`
-	Completed bool   `json:"completed"`
-}
-
-var todos = []todo{
-	{ID: "1", Item: "Ramayan", Completed: false},
-	{ID: "2", Item: "Mahabharat", Completed: false},
-	{ID: "3", Item: "Kuran", Completed: false},
-}
-
-//--------------------------<< get all todo >>---------------
-func getTodos(context *gin.Context) { //context is info of header, data etc
-	context.IndentedJSON(http.StatusOK, todos) // prepare response body(json) from struct
-
-}
-
-//--------------------------<< add todo >>---------------
-func addTodos(context *gin.Context) {
-	//need to convert data comming from post api in struct(json)
-	var newTodo todo
-	if err := context.BindJSON(&newTodo); err != nil {
-		return
-	}
-	todos = append(todos, newTodo)
-	context.IndentedJSON(http.StatusCreated, todos)
-}
-
-//--------------------------<< Find todo By Id >>---------------
-func getTodoById(id string) (*todo, error) {
-	for i, t := range todos {
-		if t.ID == id {
-			return &todos[i], nil
-		}
-	}
-	return nil, errors.New("todo not found")
-}
-
-func findTodo(context *gin.Context) {
-	// localhost:9099/todos/3 need to parse param 3
-	// all info is available in context
-	id := context.Param("id")
-	todo, err := getTodoById(id)
-	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
-		return
-	}
-	context.IndentedJSON(http.StatusOK, todo)
-}
-
-//--------------------------<< update todo >>---------------
-func toggleStatus(context *gin.Context) {
-	id := context.Param("id")
-	todo, err := getTodoById(id)
-	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
-		return
-	}
-	todo.Completed = !todo.Completed
-	context.IndentedJSON(http.StatusOK, todo)
-}
+var (
+	apiHost string         // API host
+	apiPort string         // API port
+	err     error          // error variable for error handling
+	g       errgroup.Group // Initialize the error group for goroutines
+)
 
 func main() {
-	fmt.Println("Running...")
-	//Creating a gin server
-	router := gin.Default()
-	//Create an end point which return us todos list
-	router.GET("/todos", getTodos)
-	//Create an end point which add todo in my todos list
-	router.POST("/todos", addTodos)
-	//adding dynamic end point
-	router.GET("/todos/:id", findTodo)
-	//update todo
-	router.PATCH("/todos/:id", toggleStatus)
-	//Specifying router path
-	router.Run("localhost:9099")
+	//fmt.Println(env.Env["OWNER_NAME"])
+	apiHost = env.Env["API_HOST"]
+	apiPort = env.Env["API_PORT"]
 
+	// Start the API server in a goroutine
+	g.Go(func() error {
+		return startAPIServer() // Start the API server
+	})
+	// Wait for the API server to finish
+	if err = g.Wait(); err != nil {
+		log.Fatalln(err) // Exit with error
+	}
+
+}
+
+// startAPIServer starts the API server
+func startAPIServer() (err error) {
+	var (
+		router    *gin.Engine  // gin router
+		apiServer *http.Server // HTTP server
+	)
+
+	gin.SetMode(gin.ReleaseMode) // Set the gin mode to release
+	gin.DisableConsoleColor()    // Disable the console color
+
+	router = gin.Default()              // Initialize the router
+	router.RedirectFixedPath = true     // Enable the fixed path redirect
+	router.RedirectTrailingSlash = true // Enable the trailing slash redirect
+
+	// Add the gin middleware
+	router.Use(generateTransId())
+	// API Endpoints
+	router.GET("/statistics", actions.Stat)
+	router.POST("/transactions", actions.Transactions)
+	router.DELETE("/transactions", actions.Delete)
+
+	// apierver contains instance details for the server
+	apiServer = &http.Server{
+		Addr:         apiHost + ":" + apiPort, // Set the address (host:port)
+		Handler:      router,                  // Set the router
+		ReadTimeout:  60 * time.Second,        // Set the read timeout
+		WriteTimeout: 60 * time.Second,        // Set the write timeout
+	}
+
+	fmt.Println("Starting API server on " + apiHost + ":" + apiPort)
+	logs.Info("", "starting api server on "+apiHost+":"+apiPort, "")
+	// Listen and serve on the host and port specified
+	return apiServer.ListenAndServe()
+}
+
+// generateTransId is used to generate a unique transaction id for each request
+func generateTransId() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		UUID, _ := uuid.NewRandom() // Generate a new UUID
+		c.Set("transId", UUID.String())
+	}
 }
